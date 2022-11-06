3a6421c7fb44b21e5aa030d903a6d13325c849ab
Revo <ganandocriptomonedass@gmail.com>
2022-11-06 17:42:12 -0500

    "Empece a usar pycharm porque me daba error los interpretes en VSCODE"

--- app/main.py
+++ app/main.py
@@ -1,22 +1,31 @@
 import read_csv
 import grafica
 import utils
+import pandas as pd
 
 
 def run():
-  data = read_csv.read_csv('data.csv')
+    """
   data = list(filter(lambda item : item ["Continent"] == "South America",data))
   labels = list(map(lambda x: x["Country/Territory"],data))
   values = list(map(lambda x: x["World Population Percentage"], data))
   grafica.generate_pie_chart(labels, values)
+  """
+    df = pd.read_csv("data.csv")
+    df = df[df["Continent"] == "South America"]
+    labels = df["Country/Territory"].values
+    values = df["World Population Percentage"].values
+    grafica.generate_pie_chart(labels, values)
 
-  country = input("Escribe el pais =>").capitalize()
-  result = utils.population_by_country(data, country)
+    data = read_csv.read_csv('data.csv')
+    country = input("Escribe el pais =>").capitalize()
+    result = utils.population_by_country(data, country)
 
-  if len(result) > 0:
-    country = result[0]
-    labels, values = utils.get_population(country)
-    grafica.generate_bar_chart(country["Country/Territory"], labels, values)
+    if len(result) > 0:
+        country = result[0]
+        labels, values = utils.get_population(country)
+        grafica.generate_bar_chart(country["Country/Territory"], labels, values)
+
 
 if __name__ == "__main__":
-  run()
+    run()
